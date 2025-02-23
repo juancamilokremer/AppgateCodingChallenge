76e1f7f046a7d962da2b6747aabb9b43f2f0104f
Juan Camilo Kremer <juancamilokremer@gmail.com>
2025-02-23 10:19:35 -0500

    Se agrega la clase principal de spring boot

---
+++ src/main/java/com/appgate/coding/challenge/exercise/AppgateCodingChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.appgate.coding.challenge.exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppgateCodingChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppgateCodingChallengeApplication.class, args);
+	}
+
+}
