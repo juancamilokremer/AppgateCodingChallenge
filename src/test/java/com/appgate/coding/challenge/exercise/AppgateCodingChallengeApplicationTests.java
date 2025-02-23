e38459fbe6051f8d50e0aa1a5ae8f8a6858ba3d6
Juan Camilo Kremer <juancamilokremer@gmail.com>
2025-02-23 10:21:47 -0500

    Se agrega la clase de test de la clase principal de spring boot

---
+++ src/test/java/com/appgate/coding/challenge/exercise/AppgateCodingChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appgate.coding.challenge.exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppgateCodingChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
